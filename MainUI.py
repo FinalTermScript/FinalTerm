ae952d5180b7d9673ba61d8f04ca7fc4ea59eda5
kakiku <39730096+kakik@users.noreply.github.com>
2021-05-22 23:59:53 +0900

    ui text font 수정

--- MainUI.py
+++ MainUI.py
@@ -7,6 +7,7 @@ import folium
 import googlemaps
 
 
+
 import urllib.request
 import requests
 from PIL import Image
@@ -43,29 +44,30 @@ class Interface:
 
         self.canvas.create_line(300, 400, 500, 400)
 
+        self.temp_font=font.Font(size=10, weight='bold', family='italic')
 # ----------------------------------- 여긴 계열 선택 ----------------------------------------------\
         self.brand = ["전체", "인문계열", "사회계열", "교육계열", "공학계열", "자연계열", "의약계열", "예체능계열"]
         self.str1 = StringVar()
         self.str1.trace('w', self.changeMajor)
-        self.canvas.create_text(40, 40, text="계열")
+        self.canvas.create_text(40, 40, text="계열",font=self.temp_font)
         self.line_select = Combobox(self.window, state='readonly', textvariable=self.str1, values=self.brand)
         self.line_select.place(x=100, y=30)
 
 # ----------------------------------- 여긴 학과 선택 ----------------------------------------------
-        self.canvas.create_text(40, 80, text="학과")
+        self.canvas.create_text(40, 80, text="학과",font=self.temp_font)
         self.major_select = Combobox(self.window)
         self.major_select['value'] = self.line_list  # 학과를 xml로 로드해서 가져와야함 (리스트로 받는다)
         self.major_select.place(x=100, y=70)
 
 #----------------------------------- 여긴 OO도 선택지역 ----------------------------------------------
-        self.canvas.create_text(40, 120, text="지역-도")
+        self.canvas.create_text(40, 120, text="지역-도",font=self.temp_font)
         self.area_select = Combobox(self.window)
         self.area_select['value'] = ("서울특별시", "인천광역시", "부산광역시", "대전광역시", "대구광역시", "광주광역시", "울산광역시", "경기도",
                                        "충청북도", "충청남도", "경상북도", "경상남도", "강원도", "전라북도", "전라남도", "제주도")
         self.area_select.place(x=100, y=110)
 
 #----------------------------------- 여긴 OO시 선택지역 ---------------------------------------------
-        self.canvas.create_text(40, 160, text="지역-시")
+        self.canvas.create_text(40, 160, text="지역-시",font=self.temp_font)
         self.country_select = Combobox(self.window)
         self.country_select['value'] = None
         self.country_select.place(x=100, y=150)
